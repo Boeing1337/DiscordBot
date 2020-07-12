0c81001631fe187174a5953a6f8d9cc5e6d33a01
Boeing1337 <56965556+Boeing1337@users.noreply.github.com>
2020-07-13 01:36:51 +0300

    123

--- src/main/java/dungeon/VaranApplication.java
+++ src/main/java/dungeon/VaranApplication.java
@@ -15,7 +15,6 @@ public class VaranApplication {
     }
 
     public static void main(String[] args) {
-
         SpringApplication.run(VaranApplication.class, args);
         botInit.botInit();
     }
